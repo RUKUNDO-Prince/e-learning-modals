b68220775d3453ade93fea953e1cc3a1f80aadcb
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-02-10 06:33:28 +0200

    edited the read pdf modal, adding more features

--- generate-subtitles.py
+++ generate-subtitles.py
@@ -1,11 +1,14 @@
 import assemblyai as aai
+from tkinter.filedialog import *
 
 aai.settings.api_key = "6183b57b93b1460a91e69f4210a464f7"
+
+file = askopenfilename()
 
 transcript = aai.Transcriber().transcribe("meddy.mp4")
 
 subtitles = transcript.export_subtitles_srt()
 
-f = open("subtitles.srt", "a")
+f = open(file, "a")
 f.write(subtitles)
 f.close()
--- read-pdf.py
+++ read-pdf.py
@@ -1,18 +1,29 @@
 import PyPDF2
 import pyttsx3
 from tkinter.filedialog import *
+import pdfplumber
 
 file = askopenfilename()
 
 pdfReader = PyPDF2.PdfReader(file)
-# pages = pdfReader.numPages
 pages = len(pdfReader.pages)
 
-for num in range(0, pages):
-    page = pdfReader.pages[num]
-    text = page.extract_text()
-    player = pyttsx3.init()
-    player.say(text)
-    # print("the texts are: {}".format(text))
-    player.runAndWait()
+# for num in range(0, pages):
+#     page = pdfReader.pages[num]
+#     text = page.extract_text()
+#     player = pyttsx3.init()
+#     player.say(text)
+#     # print("the texts are: {}".format(text))
+#     player.runAndWait()
 
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        # print(text)
+        s = pyttsx3.init()
+        s.save_to_file(text, 'file-audio.mp3')
+        s.say(text)
+        s.runAndWait()
+
+file.close()
